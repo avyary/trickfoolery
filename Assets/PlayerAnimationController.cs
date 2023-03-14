dc71065bc596760006e589f5df228b153077000d
lukey <71182925+lukurd@users.noreply.github.com>
2023-03-14 13:06:32 -0400

    mushroom prefab delete

--- Assets/PlayerAnimationController.cs
+++ Assets/PlayerAnimationController.cs
@@ -3,49 +3,46 @@ using UnityEngine;
 public class PlayerAnimationController : MonoBehaviour
 {
     [SerializeField] private Animator animator;
-    [SerializeField] private AnimationClip moveForwardAnimation;
-    [SerializeField] private AnimationClip moveLeftAnimation;
-    [SerializeField] private AnimationClip moveBackwardAnimation;
-    [SerializeField] private AnimationClip moveRightAnimation;
-    [SerializeField] private AnimationClip dashAnimation;
-    [SerializeField] private AnimationClip tauntAnimation;
-    [SerializeField] private AnimationClip idleAnimation;
+    [SerializeField] private AnimationClip tomRunning;
+    [SerializeField] private AnimationClip tomRolling;
+    [SerializeField] private AnimationClip tomTaunt;
+    [SerializeField] private AnimationClip tomIdle;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
-        animator.Play(idleAnimation.name);
+        animator.Play(tomIdle.name);
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            animator.Play(moveForwardAnimation.name);
+            animator.Play(tomRunning.name);
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
-            animator.Play(moveLeftAnimation.name);
+            animator.Play(tomRunning.name);
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            animator.Play(moveBackwardAnimation.name);
+            animator.Play(tomRunning.name);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            animator.Play(moveRightAnimation.name);
+            animator.Play(tomRunning.name);
         }
         else if (Input.GetKeyDown(KeyCode.Space))
         {
-            animator.Play(dashAnimation.name);
+            animator.Play(tomRolling.name);
         }
         else if (Input.GetKeyDown(KeyCode.F))
         {
-            animator.Play(tauntAnimation.name);
+            animator.Play(tomTaunt.name);
         }
         else if (!Input.anyKey)
         {
-            animator.Play(idleAnimation.name);
+            animator.Play(tomIdle.name);
         }
     }
 }
