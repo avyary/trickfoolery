c48f07f5f720c11282aa826e9498e9d8f28620dc
Jimmy Hao <64294214+hzc011213@users.noreply.github.com>
2023-04-03 15:37:50 -0400

    Update ActivateObjectOnTrigger.cs

--- Assets/Scripts/ActivateObjectOnTrigger.cs
+++ Assets/Scripts/ActivateObjectOnTrigger.cs
@@ -8,12 +8,18 @@ public class ActivateObjectOnTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        float local_hype;
+        
         // Check if the other object is the player
         if (other.CompareTag("Player"))
-        {
-            // Activate the two objects
-            activateObject1.SetActive(true);
-            activateObject2.SetActive(true);
+        {  
+            // Check if the hype bar reach the threshold within this level 
+            if (local_hype >= 30)
+            {
+                // Activate the objects
+                activateObject1.SetActive(true);
+                activateObject2.SetActive(true);
+            }
         }
     }
 }
