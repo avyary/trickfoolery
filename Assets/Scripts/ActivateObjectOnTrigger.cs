151916fa610c358032e0732bfb3a1e0ad5169648
Isaac Wetmore <78102605+isaacWetmore@users.noreply.github.com>
2023-04-03 21:11:28 -0400

    Changes to allow level 2 progression

--- Assets/Scripts/ActivateObjectOnTrigger.cs
+++ Assets/Scripts/ActivateObjectOnTrigger.cs
@@ -1,10 +1,27 @@
 
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ActivateObjectOnTrigger : MonoBehaviour
 {
     public GameObject activateObject1;
     public GameObject activateObject2;
+    private HypeManager hypeManager;
+    private float currHype;
+    
+    [SerializeField]
+    private float hypeCheck;
+
+    [SerializeField]
+    float hype_needed;
+    
+    
+
+    private void Start()
+    {
+        hypeManager = GameObject.Find("Game Manager").GetComponent<HypeManager>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,6 +31,20 @@ public class ActivateObjectOnTrigger : MonoBehaviour
             // Activate the two objects
             activateObject1.SetActive(true);
             activateObject2.SetActive(true);
+
+            currHype = hypeManager.GetHype();
+            hypeCheck = currHype + hype_needed;
         }
+    }
+
+    private void Update()
+    {
+        if (hypeManager.GetHype() >= hypeCheck)
+        {
+            Debug.Log("Bridge destroyed");
+            activateObject1.SetActive(false);
+            activateObject2.SetActive(false);
+        }
+        
     }
 }
